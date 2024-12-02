5b00fdad8503ce6c87ad6b3f8f11a70bf54ec7c4
novo <savannapassarela@gmail.com>
2024-12-02 04:43:23 -0300

    Up

--- src/components/Scripts/leds/LedControl.jsx
+++ src/components/Scripts/leds/LedControl.jsx
@@ -6,7 +6,7 @@ const LedControl = () => {
 
   useEffect(() => {
     // Inicializar conexão WebSocket
-    const ws = new WebSocket("wss://aly137.vercel.app/ledcontrol:80");
+    const ws = new WebSocket("wss://aly137.vercel.app/ledcontrol");
 
     ws.onopen = () => {
       setConnectionStatus("Conectado!");
